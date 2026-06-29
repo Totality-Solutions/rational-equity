da8af602e3c6a76114cee0a78bd88642155992a5
khushboo-totality <khushboo@totality.solutions>
2026-06-29 14:58:18 +0530

    hide hero from thought center

--- src/app/thought-center/page.tsx
+++ src/app/thought-center/page.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 export default function ThoughtCenterPage() {
   return (
     <main className="bg-white">
-        <Hero />
+        {/* <Hero /> */}
         <HeadingSection />
         <ThoughtCenterArticles />
         {/* <InvestCTASection /> */}
